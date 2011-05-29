c9b906b078ac59aa4d1013fdd7495da60e7a71a0
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-05-29 22:46:09 +0200

    TachoPilot -> DifferentialPilot.

--- src/de/dhbw/nxt/Main.java
+++ src/de/dhbw/nxt/Main.java
@@ -9,11 +9,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		// Tachopilot nimmt den Durchmesser und Abstand der RŠder.
+		// DifferentialPilot nimmt den Durchmesser und Abstand der RŠder.
 		// Dabei wird der Abstand der RŠder von AUSSEN gemessen.
 		// Bewegungen sind aber sehr ungenau... :(
-		TachoPilot pilot = new TachoPilot(5.6f, 11.2f, Motor.A, Motor.B);
-		pilot.setSpeed(200);
+		DifferentialPilot pilot = new DifferentialPilot(5.6f, 11.2f, Motor.A, Motor.B);
+		pilot.setAcceleration(400);
 		pilot.rotate(90);
 	}
 }
