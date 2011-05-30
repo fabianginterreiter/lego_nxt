24c572b210449a75a1dc02a10de4547be2d74ec6
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-05-30 15:46:24 +0200

    Roboter moves now according to the pathfinder.

--- src/de/dhbw/nxt/Main.java
+++ src/de/dhbw/nxt/Main.java
@@ -13,6 +13,28 @@ public class Main {
 		Robot robot = new Robot();
 		robot.calibrate();
 
+		Map map = new Map(3, 3);
+		map.tileAt(1, 1).setNotPassable();
+
+		int[] currentPos = { 0, 0 };
+		
+		int[][] path = map.findPath(0, 0, 3, 3);
+		for(int i = 0; i < path.length; i++) {
+			if (currentPos[0] < path[i][0]) {
+				robot.moveToEastField();
+			} else if (currentPos[0] > path[i][0]) {
+				robot.moveToWestField();
+			}
+			
+			if (currentPos[1] < path[i][1]) {
+				robot.moveToSouthField();
+			} else if (currentPos[1] > path[i][1]) {
+				robot.moveToNorthField();
+			}
+			
+			currentPos = path[i];
+		}
+		
 		robot.moveToNorthField();
 		robot.moveToNorthField();
 
