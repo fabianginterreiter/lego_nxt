86f49abe7ff327358bed5a0b14bd480e3f3fb207
Arthur Schreiber <schreiber.arthur@gmail.com>
2011-06-12 19:41:04 +0200

    JobQueue example.

--- src/de/dhbw/nxt/Main.java
+++ src/de/dhbw/nxt/Main.java
@@ -6,11 +6,19 @@ public class Main {
 	 */
 	public static void main(String[] args) throws Exception {
 		Map map = new Map(3, 3);
-		map.tileAt(1, 1).setNotPassable();
 
 		Robot robot = new Robot(map);
 		robot.calibrate();
 
-		robot.moveTo(2, 2);
+//		map.tileAt(1, 0).setTemporarilyNotPassable();
+
+//		robot.moveTo(2, 0);
+//		robot.moveTo(0, 0);
+//		robot.moveTo(2, 0);
+
+		Job j = new Job(2, 2, 0, 1);
+		robot.getJobs().addJob(j);
+		
+		robot.processQueue();
 	}
 }
